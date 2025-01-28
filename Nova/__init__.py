53208430a5764fb3ffaa42eab2cb700fa984b76d
AmenadielXd <rv1858000@gmail.com>
2025-01-28 17:54:10 +0530

    Update __init__.py

--- Nova/__init__.py
+++ Nova/__init__.py
@@ -1,4 +1,6 @@
 import os
 
-
+if ENV:
+    API_ID = int(os.environ.get("API_ID", None))
+    API_HASH = os.environ.get("API_HASH", None)
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Phoenix_HLP")
