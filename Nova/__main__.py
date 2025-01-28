aa10775724a08ef52118656b90b0e4f8a8c6197d
AmenadielXd <rv1858000@gmail.com>
2025-01-28 17:47:12 +0530

    Update Nova

---
+++ Nova/__main__.py
@@ -0,0 +1,15 @@
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴄʜᴀᴛ",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text="ɢᴜɪᴅᴇ", callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url=config.SUPPORT_GROUP),
+        ],
+    ]
+    return buttons
