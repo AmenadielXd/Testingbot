97b66fe8c1f584b5e796f0a0dc54dfce9b7d52d2
AmenadielXd <rv1858000@gmail.com>
2025-01-28 17:50:22 +0530

    Update __main__.py

--- Nova/__main__.py
+++ Nova/__main__.py
@@ -8,8 +8,8 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text="ɢᴜɪᴅᴇ", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
         ],
     ]
     return buttons
