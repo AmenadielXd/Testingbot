5194b03e9cc716a779269dc31b11b3f13bb60b63
AmenadielXd <rv1858000@gmail.com>
2025-01-28 17:57:49 +0530

    Update __init__.py

--- Nova/__init__.py
+++ Nova/__init__.py
@@ -3,6 +3,8 @@ import os
 import sys
 import time
 
+ENV = bool(os.environ.get("ENV", False))
+
 if ENV:
     API_ID = int(os.environ.get("API_ID", None))
     API_HASH = os.environ.get("API_HASH", None)
